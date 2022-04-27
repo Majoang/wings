829ace27a703a70f3d8ac0dab956657ec8f4c521
Sello <sellomajoang76@gmail.com>
2022-04-27 13:45:27 +0930

    changes

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.circle}>
+
+     </View>
+         <Text style={styles.text}>DELICIOUS FOOD AT LOW PRICES</Text>
+         <Text>Get delicious food at low price at our cafee 
+               and enjoy!!
+         </Text>
+      <View style={styles.rec}>
+        <TouchableOpacity>
+         <Text style={styles.footer}> ENJOY THE APP</Text>
+         </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#AB47BC',
+    borderRadius:8,
+    alignItems:'center'
+    
   },
+
+  circle:
+  {
+    
+    width:300,
+    height:300,
+    borderRadius:150,
+    backgroundColor:'white',
+    marginTop:30
+    
+  },
+  rec:
+  {  
+    
+    width:300,
+    height:50,
+    borderRadius:10,
+    alignItems:'center',
+    backgroundColor:'white',
+    marginTop:245,
+  
+  },
+  text:
+  {
+    marginTop:40,
+    fontSize:20,
+    fontWeight:'bold',
+    marginBottom:20
+  },
+  footer:
+  {
+    fontSize:30,
+  fontWeight:'bold',
+  marginTop:16
+  }
 });
