996cfd18db327e224e50639dd000d4e200fda73b
Sello <sellomajoang76@gmail.com>
2022-04-29 00:39:58 +0930

    chnges

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native';
 import {useState} from 'react';
-import Contact from './Contact';
+import Getdetails from './Detail';
+import { Ionicons } from '@expo/vector-icons';
+import Getbreakfast from '../Combo';
 
 export default function App() {
-  const [totalAge, setTotalAge] = useState(0);
+  const [totalP, setTotalprice] = useState(10);
 
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 24}}>The total age is {totalAge}</Text>
-      <Contact name="Thabo Lebese" phoneNumber="59702211" totalAge={totalAge} setTotalAge={setTotalAge} />
-      <Contact name="Liteboh Neos" phoneNumber="62803322" totalAge={totalAge} setTotalAge={setTotalAge} />
+      
+      
+      <Image 
+                    style={{width:270, height:270,marginTop:15,borderRadius:50}}
+                    source={require('./logo.png')}/>
+     
+       <View style={{marginTop:10,color:'#FF6F00'}}>GET DELICIOUS FOOD FROM WINGS CAFEE!!</View>
+        <Text style={{fontSize:20,marginTop: 30,color:'#FF6F00'}}>We are selling food that you can enjoy  </Text>  
+         <Text style={{fontSize:20,marginTop: 10,color:'#FF6F00'}}>most come and get yours</Text>   
+      <View  style={styles.rectangle}>
+        <TouchableOpacity>
+         <Text style={{color:'red',fontSize:20,fontWeight:'bold',marginLeft:58,marginTop:10}}>COME & ENJOY FOOD</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity>
+      <Text style={{color:'white'}}>Home</Text>
+      <Ionicons name="home" size={35} color="white"/>
+      
+      </TouchableOpacity>
+
+      <Text style={{fontSize: 30, fontStyle:'bold'}}>THE TOTAL PRICE IS : {totalP}</Text>
+      <Getdetails name="Energy drink"  price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
+      <Getbreakfast name="Breakfast combo" price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails name="Lunch" price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
     </View>
   );
 }
@@ -17,8 +40,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FDD835',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  circle:
+{
+  backgroundColor: 'black',
+  width:200,
+  height:200,
+  borderRadius: 150,
+  marginTop:20
+},
+rectangle:
+{
+width:350,
+height:40,
+borderRadius:50,
+backgroundColor:'#FF9800',
+marginTop:230,
+marginBottom:5
+},
 });
---
+++ Detail.js
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import {useState} from 'react'
+
+export default function Getdetails({name, price, totalP, setTotalprice}) {
+  const [prices, setAge] = useState(0);
+  function Increaseprice(){
+    setAge(prices + 1)
+    setTotalprice(totalP + 1);
+  }
+  return (
+    <View style={{backgroundColor:'#FFA000',width: '100%',height:200}}>
+    <View style={styles.container}>
+                   <Image 
+                    style={styles.image}
+                    source={require('./red2.png')}/>
+                   <View style={{marginLeft:12}}>
+                   <Text style={{fontSize:18, fontWeight:'bold'}}>{name}</Text>
+                   <Text>PRICE: {prices}</Text>
+                   <Text> PRICE: {price}</Text>
+                   <Button title="INCREASE THE PRICE" onPress={Increaseprice} />
+                   </View>
+
+
+                   
+    </View>
+    </View>
+
+
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+    padding: 10,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#999',
+    borderStyle:'dashed',
+    borderWidth: 3,
+    borderColor: 'red',
+    borderRadius: 10,
+    backgroundColor:'#FFCA28'
+  },
+
+  image: {
+    height: 100,
+    width: 100,
+  },
+ 
+})
