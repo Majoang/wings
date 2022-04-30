fe1180dd612bb128b15c19923eb76c2f856c6d47
Sello <sellomajoang76@gmail.com>
2022-05-01 05:05:40 +0930

    changes

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,Image, ScrollView, } from 'react-native';
 import {useState} from 'react';
 import Getdetails from './Detail';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons,Entypo } from '@expo/vector-icons';
+
 
 
 export default function App() {
     var imagee1=require('./441-4414526_2-breakfast-toast-sandwich-combo-white-castle-breakfast.png');
     var imagee2=require('./pngtree-breakfast-takeaway-fried-fast-food-combo-png-image_2665428.jpg');
     var imagee3=require('./Price-crunch2.jpg');
+    var imagee4=require('./drinks.png');
+    var imagee5=require('./lunch food.jpg');
+    var imagee6=require('./motoho.jpg');
+    var imagee7=require('./water.png');
 
-  const [totalP, setTotalprice] = useState(10);
+  const [totalP, setTotalprice] = useState(0);
 
   return (
+    <ScrollView>
     <View style={styles.container}>
       
       
@@ -19,7 +25,8 @@ export default function App() {
                     style={{width:270, height:270,marginTop:15,borderRadius:50}}
                     source={require('./logo.png')}/>
      
-       <View style={{marginTop:10,color:'#FF6F00'}}>GET DELICIOUS FOOD FROM WINGS CAFEE!!</View>
+       
+       <Text style={{marginTop:10,color:'#FF6F00'}}>GET DELICIOUS FOOD FROM WINGS CAFEE!!</Text>
         <Text style={{fontSize:20,marginTop: 30,color:'#FF6F00'}}>We are selling food that you can enjoy  </Text>  
          <Text style={{fontSize:20,marginTop: 10,color:'#FF6F00'}}>most come and get yours</Text>   
       <View  style={styles.rectangle}>
@@ -33,11 +40,22 @@ export default function App() {
       
       </TouchableOpacity>
 
-      <Text style={{fontSize: 30, fontStyle:'bold'}}>THE TOTAL PRICE IS : {totalP}</Text>
-      <Getdetails picture={imagee1} name="Energy drink"  price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
-      <Getdetails picture={imagee2} name="Breakfast combo" price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
-      <Getdetails picture={imagee3} name="Lunch" price="10.00" totalP={totalP} setTotalprice={setTotalprice} />
+      <Text style={{fontSize: 30, fontStyle:'bold'}}>THE TOTAL PRICE IS : M{totalP}</Text>
+      <Getdetails picture={imagee1} name="Breakfast combo1"  price={40} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee2} name="Breakfast combo2" price={30} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee3} name="SNAKS" price={2} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee4} name="Soft Drinks " price={20} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee5} name="LUNCH FOOD " price={25} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee6} name="MOTOHO " price={12} totalP={totalP} setTotalprice={setTotalprice} />
+      <Getdetails picture={imagee7} name="WATER " price={8} totalP={totalP} setTotalprice={setTotalprice} />
+      <TouchableOpacity>
+      <Entypo name="arrow-bold-down" size={35} color="white"/>
+      </TouchableOpacity>
+      <View style={styles.rec}>
+      <Text style={{color:'red',fontSize:20,fontWeight:'bold',marginLeft:85,marginTop:10}}>YOUR WELCOME</Text>
+      </View>
     </View>
+    </ScrollView>
   );
 }
 
@@ -64,5 +82,13 @@ borderRadius:50,
 backgroundColor:'#FF9800',
 marginTop:230,
 marginBottom:5
+},
+rec:
+{
+ backgroundColor:'#FF9800',
+ width:350,
+height:40,
+marginTop:8,
+borderRadius:50
 },
 });
--- Detail.js
+++ Detail.js
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
 import {useState} from 'react'
 
 export default function Getdetails({name, price,picture, totalP, setTotalprice}) {
   const [prices, setAge] = useState(0);
-  function Increaseprice(){
-    setAge(prices + 1)
-    setTotalprice(totalP + 1);
+  function BUY_ITERMS(){
+    
+    setTotalprice(totalP + price);
+  }
+  function DEDUCT_ITERM(){
+    
+    setTotalprice(totalP -price);
   }
   return (
+    <ScrollView>
     <View style={{backgroundColor:'#FFA000',width: '100%',height:200}}>
     <View style={styles.container}>
                    <Image 
@@ -15,16 +20,19 @@ export default function Getdetails({name, price,picture, totalP, setTotalprice})
                     source={picture}/>
                    <View style={{marginLeft:12}}>
                    <Text style={{fontSize:18, fontWeight:'bold'}}>{name}</Text>
-                   <Text>PRICE: {prices}</Text>
-                   <Text> PRICE: {price}</Text>
-                   <Button title="INCREASE THE PRICE" onPress={Increaseprice} />
+                   <Text>PRICE : M{prices}</Text>
+                   <Text> ACTUAL PRICE : M{price}</Text>
+                   <View style={{marginBottom:2,}}>
+                   <Button title="BUY ITERMS" onPress={BUY_ITERMS} />
+                   </View>
+                   <Button title="DEDUCT ITERM" onPress={DEDUCT_ITERM} />
                    </View>
 
 
                    
     </View>
     </View>
-
+    </ScrollView>
 
 
   )
@@ -41,12 +49,19 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'red',
     borderRadius: 10,
-    backgroundColor:'#FF9800'
+    backgroundColor:'#FF9800',
+    width:330,
+    height:175,
+    marginBottom:8
   },
 
   image: {
-    height: 100,
-    width: 100,
+    height: 140,
+    width: 140,
+    borderRadius:8,
+    borderStyle:'double',
+    borderWidth:5,
+    borderColor:'#BF360C'
   },
  
 })
